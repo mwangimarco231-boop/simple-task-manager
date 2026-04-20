31a5175368642fe0723c29f6c4993b99763f8e18
Marcos Mwangi <mwangimarco231@gmail.com>
2026-04-20 12:10:15 +0300

    add settings

--- backend/backend_project/backend_project/settings.py
+++ backend/backend_project/backend_project/settings.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
